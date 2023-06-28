814b07ca3ca5adf7a5d49fe71c2cf914d35e69fb
Thiago Porfirio | Zanini Renk <thiago.porfirio@zaninirenk.com.br>
2023-06-28 14:41:52 -0300

    linkados todos os icons da pg e botoes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,21 +1,39 @@
 import React from "react";
 import Image from "../assets/avatar.svg";
-import { FaGithub, FaYoutube, FaDribbble } from "react-icons/fa";
+import { FaGithub, FaYoutube, FaDribbble, FaLinkedin } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
+import { Link } from "react-scroll";
+
 
 const Banner = () => {
   return (
-    <section className="section " id="home">
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center: "
+      id="home"
+    >
       <div className="container mx-auto">
-        <div>
+        <div className="flex felx-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
           {/* txt */}
-          <div>
-            <h1>
+          <div className="flex-1 text-center font-secondary lg:text-left mt-8">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[110px] "
+            >
               THIAGO <span>PORFIRIO</span>
-            </h1>
-            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1] ">
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary 
+              font-semibold uppercase leading-[1]"
+              >
               <span className=" text-white mr-4">Eu sou um</span>
               <TypeAnimation
                 sequence={[
@@ -31,10 +49,39 @@ const Banner = () => {
                 wrapper="span"
                 repeat={Infinity}
               />
+            </motion.div>
+            <p className=" mb-8 max-w-lg mx-auto lg:mx-0">
+              Programador autodidata, futuro Engenheiro de Software, venho
+              desenvolvendo projetos como Desenvolvedor Front-end e estou me
+              especializando nesta área, com foco em tecnologias como
+              JavaScript, ReactJS, NextJs, TypeScript e NodeJS nas horas vagas.
+            </p>
+            <div
+              className="flex max-w-max gap-x-6 items-center mb-12
+            mx-auto lg:mx-0 "
+            >
+              <button className="btn btn-lg"><Link to="contact" activeClass="active" smooth={true} spy={true}>Contato</Link></button>
+              <a href="#" className="text-gradient btn-link">
+                Meu Portifolio
+              </a>
+            </div>
+            <div
+              className="flex text-[2] gap-x-6 max-w-max mx-auto
+                lg:mx-0"
+            >
+              <a href="#">
+                <Link to="https://github.com/thiagoxporfirio"><FaGithub /></Link>
+              </a>
+              <a href="#">
+                <Link to=""><FaYoutube /></Link>
+              </a>
+              <a href="#">
+                <Link href="https://www.linkedin.com/in/thiagoxporfirio/"><FaLinkedin /></Link>
+              </a>
             </div>
           </div>
           {/* image */}
-          <div>
+          <div className="hidden lg:flex felx-1 max-w-[320px] lg:max-w-[482px] ">
             <img alt="" />
           </div>
         </div>
