3727c71dfa5760cff8982d5f3c269bb5ecd52d4a
Thiago Porfirio | Zanini Renk <thiago.porfirio@zaninirenk.com.br>
2023-07-03 17:05:01 -0300

    modificação

--- src/components/About.js
+++ src/components/About.js
@@ -1,89 +1,97 @@
-import React from 'react';
+import React from "react";
 
-import CountUp from 'react-countup'
-import { useInView } from 'react-intersection-observer'
-import { motion } from 'framer-motion';
-import { fadeIn } from '../variants';
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+import { Link } from "react-scroll";
 
 const About = () => {
   const [ref, inView] = useInView({
     threshold: 0.5,
-
-  })
-
+  });
 
   return (
-    <section className='section' id='about' ref={ref}>
-      <div className='container mx-auto'>
-        <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
           {/* img */}
           <motion.div
-          variants={fadeIn('rigth', 0.3)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{ once: false, amount: 0.3 }}
-          className='flex-1 bg-about bg-contain bg-no-repeat h-[640px]
-          mix-blend-lighten bg-top'></motion.div>
+            variants={fadeIn("rigth", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px]
+          mix-blend-lighten bg-top"
+          ></motion.div>
           {/* txt */}
           <motion.div
-          variants={fadeIn('left', 0.5)}
-          initial='hidden'
-          whileInView={'show'}
-          viewport={{ once: false, amount: 0.3 }}
-           className='flex-1'>
-            <h2 className='h2 text-accent'>Sobre mim.</h2>
-            <h3 className='h3 mb-4'>Eu sou um Desenvolvedor Front-End com 2 anos de experiencia.</h3>
-            <p className='mb-6 '>Com minha experiência prévia como programador, adquiri habilidades em infraestrutura e desenvolvimento de aplicativos web usando JavaScript. Sou uma pessoa extrovertida que aprecia viajar e capturar momentos de paisagens magníficas através de fotografias. 
-            Tenho um autêntico gosto por me envolver em projetos e executá-los com excelência, garantindo que cada trabalho seja feito de forma impecável..</p>
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <h2 className="h2 text-accent">Sobre mim.</h2>
+            <h3 className="h3 mb-4">
+              Eu sou um Desenvolvedor Front-End com 2 anos de experiencia.
+            </h3>
+            <p className="mb-6 ">
+              Com minha experiência prévia como programador, adquiri habilidades
+              em infraestrutura e desenvolvimento de aplicativos web usando
+              JavaScript. Sou uma pessoa extrovertida que aprecia viajar e
+              capturar momentos de paisagens magníficas através de fotografias.
+              Tenho um autêntico gosto por me envolver em projetos e executá-los
+              com excelência, garantindo que cada trabalho seja feito de forma
+              impecável.
+            </p>
 
-            <div className='flex gap-x-6 lg:gap-x-10 mb-12 '>
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12 ">
               <div>
-                <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                  {
-                    inView ?
-                      <CountUp start={0} end={2} duration={6} /> : null}
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={2} duration={6} /> : null}
                 </div>
-                <div className='font-primary text-sm tracking-[2px] ' >
+                <div className="font-primary text-sm tracking-[2px] ">
                   Anos de <br />
                   Experiencia
                 </div>
               </div>
               <div>
-                <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                  {
-                    inView ?
-                      <CountUp start={0} end={15} duration={8} /> : null}
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={15} duration={8} /> : null}
                   K+
                 </div>
-                <div className='font-primary text-sm tracking-[2px] ' >
+                <div className="font-primary text-sm tracking-[2px] ">
                   Views em <br />
                   Projetos
                 </div>
               </div>
               <div>
-                <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                  {
-                    inView ?
-                      <CountUp start={0} end={5} duration={6} /> : null}
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={5} duration={6} /> : null}
                 </div>
-                <div className='font-primary text-sm tracking-[2px] ' >
+                <div className="font-primary text-sm tracking-[2px] ">
                   Projetos <br />
                   Realizados
                 </div>
               </div>
             </div>
 
-            <div className='flex gap-x-8 items-center mt-9'>
-              <button className='btn btn-lg'>Entre em contato</button>
-              <a href='#' className='text-gradient btn-link' >Meu Portifolio</a>
+            <div className="flex gap-x-8 items-center mt-9">
+              <button className="btn btn-lg">
+                <Link to="contact" activeClass="active ">
+                  Entre em contato
+                </Link>
+              </button>
+              <a href="#" className="text-gradient btn-link">
+                Meu Portifolio
+              </a>
             </div>
-
           </motion.div>
         </div>
       </div>
     </section>
-  )
-
+  );
 };
 
 export default About;
