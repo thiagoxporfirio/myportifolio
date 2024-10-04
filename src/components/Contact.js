313fef348309c2761b602fc3475d294865bfddda
thiagoxporfirio <thiagovporfirio@gmail.com>
2024-10-04 19:40:23 -0300

    add email from contact me

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
+import emailjs from "emailjs-com";
+import { ClipLoader } from "react-spinners";
 
 const Contact = () => {
+	const form = useRef();
+	const [loading, setLoading] = useState(false);
+	const [sent, setSent] = useState(false);
+
+	const sendEmail = e => {
+		e.preventDefault();
+		setLoading(true);
+
+		emailjs
+			.sendForm(
+				"service_a0mm9wl",
+				"template_f69gpxi",
+				form.current,
+				"YXfuoydDqImjYuaoN"
+			)
+			.then(
+				result => {
+					console.log(result.text);
+					setLoading(false);
+					setSent(true);
+					alert("Email enviado com sucesso!");
+					e.target.reset();
+
+					setTimeout(() => {
+						setSent(false);
+					}, 3000);
+				},
+				error => {
+					console.log(error.text);
+					setLoading(false);
+					alert("Ocorreu um erro ao enviar o email.");
+				}
+			);
+	};
+
 	return (
 		<section className="section py-16 lg:selection" id="contact">
 			<div className="container mx-auto">
 				<div className="flex flex-col lg:flex-row">
-					{/*text */}
+					{/* text */}
 					<motion.div
 						variants={fadeIn("right", 0.3)}
 						initial="hidden"
@@ -26,8 +63,10 @@ const Contact = () => {
 							</h2>
 						</div>
 					</motion.div>
-					{/*form */}
+					{/* form */}
 					<motion.form
+						ref={form}
+						onSubmit={sendEmail}
 						variants={fadeIn("left", 0.3)}
 						initial="hidden"
 						whileInView={"show"}
@@ -37,19 +76,34 @@ const Contact = () => {
 						<input
 							className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
 							type="text"
+							name="from_name"
 							placeholder="Seu nome"
+							required
 						/>
 						<input
 							className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
-							type="text"
+							type="email"
+							name="from_email"
 							placeholder="Seu email"
+							required
 						/>
-
 						<textarea
 							className="bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
-							placeholder="Sua menssagem:"
+							name="message"
+							placeholder="Sua mensagem"
+							required
 						></textarea>
-						<button className="btn btn-lg">Enviar menssagem</button>
+
+						{/* Botão de envio */}
+						<button className="btn btn-lg" type="submit" disabled={loading}>
+							{loading ? (
+								<ClipLoader color="#ffffff" size={24} />
+							) : sent ? (
+								"Mensagem Enviada!"
+							) : (
+								"Enviar mensagem"
+							)}
+						</button>
 					</motion.form>
 				</div>
 			</div>
